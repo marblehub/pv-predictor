7b332dea6ca6d0e286f83c2a7f6fae8123132cbe
marblehub <99378528+marblehub@users.noreply.github.com>
2025-07-13 00:28:11 +0200

    Update app.py

--- app.py
+++ app.py
@@ -33,5 +33,9 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)})
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # required by Render
+    app.run(host='0.0.0.0', port=port, debug=False)  # disable debug
+
